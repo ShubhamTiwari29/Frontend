9ce6683c2611a4f898ce5c4181f0f23f76387d16
Shubham29lko@gmail.com <162909471+ShubhamTiwari29@users.noreply.github.com>
2024-06-27 21:41:56 +0530

    first commit

---
+++ imagesell/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="w-full  text-red-600">
+        <h1 className=" text-center text-red-600 ">Hey this is our company website</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
