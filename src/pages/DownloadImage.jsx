3b0466ff15f7c83cff27dc9fae31c4337e5f942f
Shubham Tiwari <shubham29lko@gmail.com>
2024-11-02 21:55:50 +0530

    ..

--- src/pages/DownloadImage.jsx
+++ src/pages/DownloadImage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-import DownloadImageDisplay from '../components/downloadImage/downloadImageDisplay';
+import DownloadImageDisplay from '../components/downloadImage/downloadImageDisplay.jsx';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
 
 const DownloadImage = () => {
